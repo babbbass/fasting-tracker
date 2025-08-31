b469741e8598bdf437a9c54f0005b3268ce7965e
sebastien <sebastien.savan@gmail.com>
2025-08-31 10:16:00 +0200

    improve ux toggle theme button

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -3,50 +3,27 @@
 import * as React from "react"
 import { Flower, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
-
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
 
   return (
     <div className='flex justify-end'>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant='outline'
-            size='icon'
-            className='cursor-pointer border-primary text-primary hover:text-accent-card'
-          >
-            <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 ' />
-            <Flower className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-            <span className='sr-only'>Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent
-          align='end'
-          className='bg-background text--accent-foreground'
-        >
-          <DropdownMenuItem
-            onClick={() => setTheme("light")}
-            className='cursor-pointer hover:bg-accent hover:text-accent-foreground'
-          >
-            Clair
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            onClick={() => setTheme("dark")}
-            className='cursor-pointer hover:bg-accent hover:text-accent-foreground'
-          >
-            Système
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <Button
+        variant='outline'
+        size='lg'
+        onClick={toggleTheme}
+        className='cursor-pointer border-primary text-primary hover:text-accent-foreground'
+      >
+        <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 ' />
+        <Flower className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+        <span className='sr-only'>Toggle theme</span>
+      </Button>
     </div>
   )
 }
